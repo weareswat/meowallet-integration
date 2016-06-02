b1359f4d231e0f849e4cd05f989317ec9bd1bec2
Marcos <marcoslamuria@gmail.com>
2016-06-02 13:17:40 +0100

    fix tests when no meo-wallet-api-key on env

--- test/weareswat/meowallet_integration/handlers/generate_mb_ref_test.clj
+++ test/weareswat/meowallet_integration/handlers/generate_mb_ref_test.clj
@@ -5,21 +5,24 @@
             [result.core :as result]))
 
 (deftest basic-test
-  (testing "/payment-reference/create returns OK"
-    (let [body {:supplier {:api-key (env :meo-wallet-api-key)}
-                :amount 10
-                :currency "EUR"}
-          response (test-request/parsed-response :post "/payment-reference/create" body)
-          body (:body response)]
-      (is (:mb body))
-      (is (= 200 (:status response)))))
+  (if-not (env :meo-wallet-api-key)
+    (println "Warning: No meo wallet api key on env (ignoring test)")
 
-  (testing "/payment-reference/create returns OK"
-    (let [body {:supplier {:api-key "wqkjejkqw"}
-                :currency "EUR"}
-          response (test-request/parsed-response :post "/payment-reference/create" body)
-          body (:body response)]
-      (is (= "missing-required-key"
-             (get-in body [:errors :amount])))
-      (is (= 400 (:status response)))))
-  )
+    (do
+      (testing "/payment-reference/create returns OK"
+        (let [body {:supplier {:api-key (env :meo-wallet-api-key)}
+                    :amount 10
+                    :currency "EUR"}
+              response (test-request/parsed-response :post "/payment-reference/create" body)
+              body (:body response)]
+          (is (:mb body))
+          (is (= 200 (:status response)))))
+
+      (testing "/payment-reference/create returns OK"
+        (let [body {:supplier {:api-key "wqkjejkqw"}
+                    :currency "EUR"}
+              response (test-request/parsed-response :post "/payment-reference/create" body)
+              body (:body response)]
+          (is (= "missing-required-key"
+                 (get-in body [:errors :amount])))
+          (is (= 400 (:status response))))))))
